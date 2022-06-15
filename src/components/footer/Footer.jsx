d85e536f232d151a4260c91bcd32abd38ebcee7c
quArex-rQer <77916984+quArex-rQer@users.noreply.github.com>
2022-06-15 19:30:39 +0300

    Add files via upload

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,17 @@
 import './footer.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCoffee} from '@fortawesome/free-solid-svg-icons';
+import {faFacebookF, faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className='footerContainer'>
         <span>Anasayfa | Kullanıcı Sözleşmesi | Gizlilik Sözleşmesi</span>
+        <div className='icons'>
+          <FontAwesomeIcon icon={faFacebookF} className='icon' />
+          <FontAwesomeIcon icon={faTwitter} className='icon' />
+        </div>
       </div>
     </div>
   );
